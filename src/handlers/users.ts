34b241bd04a65e2557c1ecef0a61744b6ba41959
sitogi <takano162@gmail.com>
2023-02-02 22:37:39 +0900

    chore: init files

---
+++ src/handlers/users.ts
@@ -0,0 +1,15 @@
+import { Request } from 'express';
+
+const users = [
+  { id: 1, name: 'alice' },
+  { id: 2, name: 'bob' },
+];
+
+
+export const getUser = async (req: Request<{ id: string }>) => {
+  return users.find(u => String(u.id) === req.params.id);
+};
+
+export const getUsers = async () => {
+  return users;
+};
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { getUser, getUsers } from './handlers/users';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/user/:id', async (req, res) => {
+  try {
+    const user = await getUser(req);
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('internal error');
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('internal error');
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express listening on port ' + (process.env.PORT || 3000));
+});
