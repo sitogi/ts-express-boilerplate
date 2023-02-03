5f986ecaa7118183968af1377af9f22d221e6cfe
sitogi <takano162@gmail.com>
2023-02-03 13:50:16 +0900

    feature: Add DB operation with Prisma

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,14 +1,40 @@
+import { User } from '@prisma/client';
 import { Request } from 'express';
 
-const users = [
-  { id: 1, name: 'alice' },
-  { id: 2, name: 'bob' },
-];
+import { prisma } from '../libs/prisma';
 
-export const getUser = async (req: Request<{ id: string }>) => {
-  return users.find((u) => String(u.id) === req.params.id);
+export const getUsers = async (): Promise<User[]> => {
+  const users = await prisma.user.findMany();
+  return users;
 };
 
-export const getUsers = async () => {
-  return users;
+export const getUser = async (req: Request<{ id: string }>): Promise<User | null> => {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(req.params?.id) },
+  });
+  return user;
+};
+
+export const updateUser = async (req: Request<{ id: string }>): Promise<User | null> => {
+  const { name, email } = req.body;
+  const user = await prisma.user.update({
+    where: { id: parseInt(req.params?.id) },
+    data: { name, email },
+  });
+  return user;
+};
+
+export const createUser = async (req: Request): Promise<User | null> => {
+  const { name, email } = req.body;
+  const user = await prisma.user.create({
+    data: { name, email },
+  });
+  return user;
+};
+
+export const deleteUser = async (req: Request<{ id: string }>): Promise<User | null> => {
+  const user = await prisma.user.delete({
+    where: { id: parseInt(req.params?.id) },
+  });
+  return user;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 import express from 'express';
 
-import { getUser, getUsers } from './handlers/users';
+import { createUser, deleteUser, getUser, getUsers, updateUser } from './handlers/users';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// ❯ http POST localhost:3000/user name=new-user email=new@example.com
+app.post('/user', async (req, res) => {
+  try {
+    const user = await createUser(req);
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('internal error');
+  }
+});
+
+// ❯ http localhost:3000/users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('internal error');
+  }
+});
+
+// ❯ http localhost:3000/user/1
 app.get('/user/:id', async (req, res) => {
   try {
     const user = await getUser(req);
@@ -17,10 +40,22 @@ app.get('/user/:id', async (req, res) => {
   }
 });
 
-app.get('/users', async (req, res) => {
+// ❯ http PUT localhost:3000/user/4 name=new-user-updated email=new@example.com
+app.put('/user/:id', async (req, res) => {
   try {
-    const users = await getUsers();
-    res.status(200).json(users);
+    const user = await updateUser(req);
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('internal error');
+  }
+});
+
+// ❯ http DELETE localhost:3000/user/4
+app.delete('/user/:id', async (req, res) => {
+  try {
+    const user = await deleteUser(req);
+    res.status(200).json(user);
   } catch (err) {
     console.error(err);
     res.status(500).send('internal error');
